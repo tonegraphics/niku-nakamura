6f13cbd42d167059caf1746ffc005fc339be3dde
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-04-24 00:25:04 +0900

    fix bug about humberger

--- js/humberger_menu.js
+++ js/humberger_menu.js
@@ -2,5 +2,19 @@ $(function(){
     $('#nav_toggle').click(function(){
         $('.p-hero__header').toggleClass('open');
         $('nav').slideToggle(500);
+        console.log("hoge");
+    });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        console.log(w);
+        var x = 1024;
+        if (w >= x) {
+            $('.p-hero__header').removeClass('open');
+            $('nav').css('display', 'block');
+        }else {
+            $('nav').css('display', 'none');
+        }
     });
 });
+
